342ab0a2b9d304d9e17746ef031a05aeb95e2c6c
cit007 <itcho0077@gmail.com>
2020-12-26 00:48:00 +0900

    query with param(id) and usage of param in resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,20 @@
-const genie = {
-  name: "genie",
-  age: 4,
-};
+const peoples = [
+  {
+    id: 1,
+    name: "genie1",
+    age: 4,
+  },
+  {
+    id: 2,
+    name: "genie2",
+    age: 8,
+  },
+];
 
 const resolvers = {
   Query: {
-    person: () => genie,
+    people: () => peoples,
+    person: (_, { id }) => peoples.filter((people) => people.id === id)[0],
   },
 };
 
